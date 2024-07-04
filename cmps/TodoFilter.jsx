0df725cf0a94d8ba76d62ceb7c470867c46364b3
Dor <dorhakim100@gmail.com>
2024-07-04 18:48:12 +0300

    Add sorting on html, no functionality

--- cmps/TodoFilter.jsx
+++ cmps/TodoFilter.jsx
@@ -1,10 +1,21 @@
-const { useState, useEffect } = React
+const { useState, useEffect, useRef } = React
 const { useSelector, useDispatch } = ReactRedux
 
 import { setFilterBy } from '../store/todo.actions.js'
 
 export function TodoFilter({ filterBy }) {
   const [filterByToEdit, setFilterByToEdit] = useState({ ...filterBy })
+
+  const allSortButtonRef = useRef()
+  const [allSortButton, setAllSortButton] = useState(allSortButtonRef.current)
+  const activeSortButtonRef = useRef()
+  const [activeSortButton, setActiveSortButton] = useState(
+    activeSortButtonRef.current
+  )
+  const doneSortButtonRef = useRef()
+  const [doneSortButton, setDoneSortButton] = useState(
+    doneSortButtonRef.current
+  )
 
   useEffect(() => {
     // Notify parent
@@ -38,6 +49,31 @@ export function TodoFilter({ filterBy }) {
     setFilterBy(filterByToEdit)
   }
 
+  function onSortTodos({ target }) {
+    const sortBy = target.id
+    const isChecked = target.checked
+
+    console.log(sortBy)
+    console.log(isChecked)
+    switch (sortBy) {
+      case 'all':
+        activeSortButtonRef.current.checked = false
+        doneSortButtonRef.current.checked = false
+        break
+      case 'active':
+        allSortButtonRef.current.checked = false
+        doneSortButtonRef.current.checked = false
+        break
+      case 'done':
+        allSortButtonRef.current.checked = false
+        activeSortButtonRef.current.checked = false
+        break
+
+      default:
+        break
+    }
+  }
+
   const { txt, importance } = filterByToEdit
   return (
     <section className='todo-filter'>
@@ -60,6 +96,35 @@ export function TodoFilter({ filterBy }) {
           id='importance'
           name='importance'
         />
+        <div className='sorting-buttons-container'>
+          <div className='sorting-container all'>
+            <input
+              type='checkbox'
+              id='all'
+              onChange={onSortTodos}
+              ref={allSortButtonRef}
+            />
+            <label htmlFor='all'>All</label>
+          </div>
+          <div className='sorting-container active'>
+            <input
+              type='checkbox'
+              id='active'
+              onChange={onSortTodos}
+              ref={activeSortButtonRef}
+            />
+            <label htmlFor='active'>Active</label>
+          </div>
+          <div className='sorting-container done'>
+            <input
+              type='checkbox'
+              id='done'
+              onChange={onSortTodos}
+              ref={doneSortButtonRef}
+            />
+            <label htmlFor='done'>Done</label>
+          </div>
+        </div>
 
         <button hidden>Set Filter</button>
       </form>
